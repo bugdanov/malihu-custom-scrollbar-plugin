8e92a515d326384b7ac96b7058925ff307269e78
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2015-10-02 07:05:03 +0200

    while scrolling disable mouseevents for iframes outside of the container also

--- jquery.mCustomScrollbar.js
+++ jquery.mCustomScrollbar.js
@@ -1204,7 +1204,7 @@ and dependencies (minified).
 				_iframe(true); /* enable iframes events */
 			});
 			function _iframe(evt){
-				var el=mCSB_container.find("iframe");
+				var el=$("iframe");
 				if(!el.length){return;} /* check if content contains iframes */
 				var val=!evt ? "none" : "auto";
 				el.css("pointer-events",val); /* for IE11, iframe's display property should not be "block" */
